9e5eb136a0e292f53b193856eec6c521b96ccecd
Phung  Buu Quang <buuquang@jv-it.com.vn>
2022-04-12 20:52:29 +0700

    promotion

--- diadiemlongkhanh/lib/screens/promotion/promotion_screen.dart
+++ diadiemlongkhanh/lib/screens/promotion/promotion_screen.dart
@@ -11,6 +11,8 @@ class PromotionScreen extends StatefulWidget {
 }
 
 class _PromotionScreenState extends State<PromotionScreen> {
+  List<String> filterDatas = ['Tất cả', 'Cơm', 'Bún', 'Phở'];
+  int _indexFilter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,32 +38,118 @@ class _PromotionScreenState extends State<PromotionScreen> {
               left: 0,
               right: 0,
               bottom: 120,
-              child: Column(
-                children: [
-                  Container(
-                    height: 280,
-                    margin: const EdgeInsets.only(
-                      top: 28,
-                      left: 16,
-                      right: 16,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 280,
+                      margin: const EdgeInsets.only(
+                        top: 28,
+                        left: 16,
+                        right: 16,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                        boxShadow: [
+                          BoxShadow(
+                            color: ColorConstant.green_shadow.withOpacity(0.12),
+                            offset: Offset(0, 15),
+                            blurRadius: 40,
+                          )
+                        ],
+                      ),
                     ),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12),
-                      boxShadow: [
-                        BoxShadow(
-                          color: ColorConstant.green_shadow.withOpacity(0.12),
-                          offset: Offset(0, 15),
-                          blurRadius: 40,
-                        )
-                      ],
-                    ),
-                  )
-                ],
+                    _buildFilterPromotionView(),
+                    ListView.builder(
+                      itemCount: 10,
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      padding: const EdgeInsets.only(top: 24),
+                      itemBuilder: (_, index) {
+                        return Container(
+                          height: 112,
+                          margin: const EdgeInsets.only(
+                            bottom: 16,
+                            left: 16,
+                            right: 16,
+                          ),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(12),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: ColorConstant.green_shadow
+                                              .withOpacity(0.12),
+                                          offset: Offset(0, 15),
+                                          blurRadius: 40,
+                                        ),
+                                      ]),
+                                ),
+                              ),
+                              Container(
+                                width: 80,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    )
+                  ],
+                ),
               ),
             )
           ],
         ),
+      ),
+    );
+  }
+
+  Container _buildFilterPromotionView() {
+    return Container(
+      height: 28,
+      margin: const EdgeInsets.only(
+        top: 48,
+        left: 16,
+        right: 16,
+      ),
+      child: ListView.builder(
+        itemCount: filterDatas.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (_, index) {
+          return Container(
+            height: 28,
+            margin: const EdgeInsets.only(right: 8),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(14),
+              color: index == _indexFilter
+                  ? ColorConstant.green_D5F4D9
+                  : Colors.transparent,
+            ),
+            child: Center(
+              child: Text(
+                filterDatas[index],
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight:
+                      index == _indexFilter ? FontWeight.w500 : FontWeight.w400,
+                  color: index == _indexFilter
+                      ? Theme.of(context).primaryColor
+                      : ColorConstant.neutral_gray,
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
